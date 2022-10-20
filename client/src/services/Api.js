016cedefaacc06c7225eb111f5263eaf3e7b7701
Ezequiel Baez <ezequielwokraczka@gmail.com>
2022-10-20 18:28:08 -0300

    fix: Added env variable for server url

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-const development = process.env.NODE_ENV !== 'production'
 
 export default () => {
     return axios.create({
-        baseURL: development ? 'http://localhost:8000/api/v1' : ''
+        baseURL: process.env.VUE_APP_SERVER_URL
     })
 }
